20921ed2ae6dd8fe7ba5467a9c55c672325eb63c
Sidharth Ramesh <tornadoalert@gmail.com>
2018-05-23 18:10:28 +0530

    changed login location

--- medblocks.py
+++ medblocks.py
@@ -10,6 +10,7 @@ import web3
 import requests
 
 w3 = web3.Web3(web3.HTTPProvider('https://ropsten.infura.io/JpLB58GjoxPYInfTNHf0')) 
+credentials_path = os.path.expanduser('~/.medblocks')
 
 @click.group()
 @click.version_option('0.01', message="MedBlocks v0.01")
@@ -81,12 +82,12 @@ def load_user(string):
     user['ethereum'] = w3.eth.account.privateKeyToAccount(user['ethereum']['private_key'])
     return user
 
-def load(file='login.json'):
+def load(file=credentials_path):
     try:
         with open(file, 'r') as f:
             user = load_user(f.read())
     except FileNotFoundError:
-        echo("[-] No user login.json file found. Define a file with the --login option")
+        echo("[-] No user logged in. Please login.")
         click.secho("Hint: Use 'createuser' to create a user json file", fg='green')
         exit(1)
     return user
@@ -120,7 +121,7 @@ def give_permission(address, medblock_index):
     pass
 
 @main.command()
-@click.option('--output','-o', help="Output file with credentials", default='login.json')
+@click.option('--output','-o', help="Output file with credentials", default='user.json')
 @click.option('--name','-n',prompt="Name of patient")
 @click.option('--phone','-p',prompt="Phone number")
 def createuser(name, phone, output):
@@ -148,51 +149,50 @@ def createuser(name, phone, output):
 def register():
     """Register current user on the blockchain"""
     user = load()
-
-
-@main.command()
-
-@click.argument('login', type=click.Path(exists=True))
-def login(login):
-    """Log in with another json file"""
-    user = load(login)
-    echo("[o] Seems like a valid user file")
-    if click.confirm(text="This will lose all previous user data. Are you sure?"):
-        echo("[+] Writing to login.json")
-        with open('login.json','w') as f:
-            f.write(dump_user(user))
-
-@main.command()
-@click.option('--login','-l', type=click.Path(), envvar='MEDBLOCKS_JSON', help="Load JSON File with user data",default='login.json')
-def info(login='login.json'):
-    """Get info about current user"""
     
-    echo("[+] Loading user from login.json")
+
+@main.command()
+@click.argument('login', type=click.Path(exists=True))
+@click.pass_context
+def login(ctx, login):
+    """Log in with a user file"""
     user = load(login)
+    if click.confirm(text="This will overwrite all previous user data. Are you sure?"):
+        echo("[+] Writing to .medblocks")
+        with open(credentials_path,'w') as f:
+            f.write(dump_user(user))
+    ctx.invoke(info)
+
+def get_ether_balance(address):
+    return w3.eth.getBalance(address)/1000000000000000000
+
+def check_ether():
+    """Sends 1 Ropsten test ether if account has 0 ETH"""
+    address = load()['ethereum'].address
+    balance = get_ether_balance(address)
+    if balance < 1:
+        secho("[o] No Ether in account. Automatically requesting test ether...", fg='yellow')
+        base = 'http://faucet.ropsten.be:3001/donate/{address}'
+        echo('[+] Requesting test ether for {}'.format(address))
+        requests.get(base.format(address=address))
+        echo("[+] Request sent. Check back after a while...")
+    else:
+        secho("[+] Got test ether! Ready to create some medblocks!", fg='green')
+    return
+
+@main.command()
+def info():
+    """Get info about current user"""
+    echo("[+] Loading user from .medblocks")
+    user = load()
     echo('Name: {}'.format(user['name']))
     echo('Phone: {}'.format(user['phone']))
     echo('RSA public key: ')
     click.secho(user['rsa'].publickey().exportKey().decode(), fg='green')
     echo('Ethereum address: {}'.format(user['ethereum'].address))
-    balance = w3.eth.getBalance(user['ethereum'].address)/1000000000000000000
+    balance = get_ether_balance(user['ethereum'].address)
     echo('Test Ether balance: {} ETH'.format(balance))
-    if balance < 1:
-        secho("[-] No Ether in account. Get ether by using the 'getether' command", fg='red')
-    else:
-        secho("[o] Private keys are valid and are ready to create some medblocks!", fg='yellow')
-
-@main.command()
-@click.option('--address','-a', help="Sends 1 test Eth to this account")
-def getether(address):
-    """Sends 1 Ropsten test ether"""
-    if address is None:
-        address = load()['ethereum'].address
-    base = 'http://faucet.ropsten.be:3001/donate/{address}'
-    echo('[+] Requesting test ether for {}'.format(address))
-    r = requests.get(base.format(address=address))
-    print(r.text)
-    echo("[+] Request sent. Check back after a while...")
-    return
+    check_ether()
 
 if __name__ == "__main__":
     main()
