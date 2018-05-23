b14cc169fa0c130ae1838bf7a0d00492e54355cb
Sidharth Ramesh <tornadoalert@gmail.com>
2018-05-24 03:11:53 +0530

    working ipfs and bigchain registration

--- medblocks.py
+++ medblocks.py
@@ -1,19 +1,21 @@
 import click
 from click import echo, secho
-from Crypto import Random
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP, AES
-import base64
 import os
 import json
-import web3
+from bigchaindb_driver import BigchainDB
+from bigchaindb_driver.crypto import generate_keypair, CryptoKeypair
 import requests
-
-w3 = web3.Web3(web3.HTTPProvider('https://ropsten.infura.io/JpLB58GjoxPYInfTNHf0')) 
+import ipfsapi
+from encryption import *
+bdb = BigchainDB(
+    'https://test.bigchaindb.com',
+    headers={'app_id': 'd5596f54',
+             'app_key': '6f108c571fcb25c4d34056bede9d246f'})
 credentials_path = os.path.expanduser('~/.medblocks')
+version = '0.01'
 
 @click.group()
-@click.version_option('0.01', message="MedBlocks v0.01")
+@click.version_option(version, message="MedBlocks v0.01")
 def main():
     """
     \b
@@ -26,49 +28,12 @@ def main():
     Store your medical records securely                                         
     """
     
-def encrypt_rsa(data, public_key):
-    handler = PKCS1_OAEP.new(public_key)
-    return base64.encodebytes(handler.encrypt(data))
-def decrypt_rsa(data, private_key):
-    handler = PKCS1_OAEP.new(private_key)
-    return handler.decrypt(base64.decodebytes(data))
 
-def pad(s):
-    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
-
-def encrypt(message, key, key_size=256):
-    message = pad(message)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return iv + cipher.encrypt(message)
-
-def decrypt(ciphertext, key):
-    iv = ciphertext[:AES.block_size]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-    return plaintext.rstrip(b"\0")
-
-def encrypt_file(file_name, key):
-    with open(file_name, 'rb') as fo:
-        plaintext = fo.read()
-    enc = encrypt(plaintext, key)
-    with open(file_name + ".enc", 'wb') as fo:
-        fo.write(enc)
-
-def decrypt_file(file_name, key):
-    with open(file_name, 'rb') as fo:
-        ciphertext = fo.read()
-    dec = decrypt(ciphertext, key)
-    with open(file_name[:-4], 'wb') as fo:
-        fo.write(dec)
-
-def generate_random_aes_key():
-    return os.urandom(32)
 
 def dump_user(user):
-    #Serialize ethereum account
-    ethereum_keys = user['ethereum']
-    user['ethereum'] = {'private_key':ethereum_keys.privateKey.hex(), 'public_key':ethereum_keys.address}
+    """Converts user dictionary to json string"""
+    bigchain_keys = user['bigchain']
+    user['bigchain'] = {'private_key':bigchain_keys.private_key, 'public_key':bigchain_keys.public_key}
     # Serialize rsa 
     rsa_key = user['rsa']
     user['rsa'] = {'private_key':rsa_key.exportKey().decode('utf-8'),'public_key':rsa_key.publickey().exportKey().decode('utf-8')}
@@ -77,12 +42,14 @@ def dump_user(user):
     return user
 
 def load_user(string):
+    """Loads json string into user dictionary"""
     user = json.loads(string)
     user['rsa'] = RSA.importKey(user['rsa']['private_key'])
-    user['ethereum'] = w3.eth.account.privateKeyToAccount(user['ethereum']['private_key'])
+    user['bigchain'] = CryptoKeypair(user['bigchain']['private_key'],user['bigchain']['public_key'])
     return user
 
 def load(file=credentials_path):
+    """Loads user dictionary from credentials_path"""
     try:
         with open(file, 'r') as f:
             user = load_user(f.read())
@@ -92,31 +59,112 @@ def load(file=credentials_path):
         exit(1)
     return user
 
-def register_on_blockchain(user):
-    rsa_public_key = user['rsa']['public_key']
-    ethereum_address = user['ethereum']['public_key']
+def register_on_blockchain(string):
+    user_dict = json.loads(string)
+    data = {
+        'schema':'medblocks.user',
+        'version':'v0.01',
+        'rsa':user_dict['rsa']['public_key'],
+        'bigchain':user_dict['bigchain']['public_key'],
+        'phone':user_dict['phone'],
+        'name':user_dict['name']
+    }
+    echo("[+] Preparing CREATE transaction with {}...".format(str(data)[:10]))
+    tx = bdb.transactions.prepare(
+    operation='CREATE',
+    signers=user_dict['bigchain']['public_key'],
+    asset={'data': data})
+
+    echo("[+] Signing with private key")
+    signed_tx = bdb.transactions.fulfill(
+    tx,
+    private_keys=user_dict['bigchain']['private_key']
+    )
+    echo("[+] Sending to the Blockchain")
+    sent = bdb.transactions.send(signed_tx)
+    echo("[+] Sent transaction {}".format(sent))
+    return sent
     # mobile => address, rsa_key
     # address => [{ ipfs hash, cipher, emergency_cipher}]
 
+current_user = load()
 
-def write_medblock(data, schema, mobile, is_emergency=False):
-    # Get ethereum address for mobile number
 
-    # Get rsa public key for ethereum address
+def filter(l, key, value):
+    new_list = []
+    for instance in l:
+        data = instance.get('data')
+        if data.get(key) == value:
+            new_list.append(instance)
+    return new_list
 
-    # Generate random aes cipher
+def version_filter(l):
+    return filter(l, 'version','v'+version)
+    
+    
+class Patient(object):
+    def __init__(self, mobile=None, public_key=None):
+        self.registered = None
+        self.records = None
+        if mobile is not None:
+            self.bio = self.get_bio_from_mobile(mobile)
+        if public_key is not None:
+            self.bio = self.get_bio_from_public_key(public_key)
+        self.medblocks = []
+        self.get_medblocks()
+        
+    def get_bio_from_mobile(self, mobile):
+        records = bdb.assets.get(search=mobile)
+        records = filter(records, 'schema','medblocks.user')
+        if len(records) == 1:
+            echo("[+] Found user with mobile number!")
+            self.registered = True
+            return records[0]['data']
+        if len(records) > 1:
+            secho("[o] Got more than one record for the same version. Last registery will be taken",fg='yellow')
+            self.registered = True
+            return records[-1]['data']
+        
+        secho("[-] No user with that phone number registered", fg='red')
+        self.registered = False
+        return
 
-    # Encrypt data with aes cipher
+    def get_bio_from_public_key(self, public_key):
+        self.records = bdb.assets.get(search=public_key)
+        registers = filter(self.records, 'schema','medblocks.user')
 
-    # Encrypt cipher with rsa key
+        if len(registers) == 1:
+            self.registered = True
+            return registers[0].get('data')
 
-    # if is_emergency:
-        # Encrypt cipher with rsa key of emregency key
+        if len(registers) > 1:
+            secho("[o] Got more than one record for the same version. Last registery will be taken",fg='yellow')
+            self.registered = True
+            return registers[-1].get('data')
+        
+        secho("[-] No user with that public key registered", fg='red')
+        self.registered = False
+        return
 
-    # write {'data':encrypted data, 'schema':schema} to ipfs and return hash
+    def get_medblocks(self):
+        if self.records is None:
+            self.records = bdb.assets.get(search=self.bio['bigchain'])
+    
+        for record in self.records:
+            if record.get('data').get('schema') == 'medblocks.medblock':
+                self.medblocks.append(record)
 
-    # return ipfs hash, encrypted cipher
-    pass
+        self.medblocks = version_filter(self.medblocks)
+        
+def ipfs_connect():
+    try:
+        api = ipfsapi.connect('127.0.0.1', 5001)
+        secho("[+] Connected to local IPFS node", fg='green')
+    except ipfsapi.exceptions.ConnectionError:
+        secho("[o] Local IPFS daemon not running. Connecting to ipfs.infura.io node...", fg='yellow')
+        api = ipfsapi.connect('https://ipfs.infura.io', 5001)
+    return api
+
 def give_permission(address, medblock_index):
     pass
 
@@ -131,15 +179,15 @@ def createuser(name, phone, output):
         'phone':phone
     }
     echo("[+] Writing Name and Phone")
-    echo("[+] Generating Ethereum keypair")
-    ethereum_keys = w3.eth.account.create()
-    user['ethereum'] = ethereum_keys
+    echo("[+] Generating BigChainDB keypair")
+    bigchain_keys = generate_keypair()
+    user['bigchain'] = bigchain_keys
     random_generator = Random.new().read
     rsa_key = RSA.generate(1024, random_generator)
     echo("[+] Generating RSA keypair")
     user['rsa'] = rsa_key
     st = dump_user(user)
-    click.secho("Writing private key data to a json file. Please be sure to save it securely. You will need it to log in later.", fg='red')
+    click.secho("Writing private key data to a json file. Please be sure to save it securely. You will need it to log in later.", fg='yellow')
     click.pause()
     echo("[+] Writing file to disk as: {}".format(output))
     with open(output,'w') as f:
@@ -149,7 +197,7 @@ def createuser(name, phone, output):
 def register():
     """Register current user on the blockchain"""
     user = load()
-    
+    register_on_blockchain(dump_user(user))
 
 @main.command()
 @click.argument('login', type=click.Path(exists=True))
@@ -163,23 +211,6 @@ def login(ctx, login):
             f.write(dump_user(user))
     ctx.invoke(info)
 
-def get_ether_balance(address):
-    return w3.eth.getBalance(address)/1000000000000000000
-
-def check_ether():
-    """Sends 1 Ropsten test ether if account has 0 ETH"""
-    address = load()['ethereum'].address
-    balance = get_ether_balance(address)
-    if balance < 1:
-        secho("[o] No Ether in account. Automatically requesting test ether...", fg='yellow')
-        base = 'http://faucet.ropsten.be:3001/donate/{address}'
-        echo('[+] Requesting test ether for {}'.format(address))
-        requests.get(base.format(address=address))
-        echo("[+] Request sent. Check back after a while...")
-    else:
-        secho("[+] Got test ether! Ready to create some medblocks!", fg='green')
-    return
-
 @main.command()
 def info():
     """Get info about current user"""
@@ -188,11 +219,42 @@ def info():
     echo('Name: {}'.format(user['name']))
     echo('Phone: {}'.format(user['phone']))
     echo('RSA public key: ')
-    click.secho(user['rsa'].publickey().exportKey().decode(), fg='green')
-    echo('Ethereum address: {}'.format(user['ethereum'].address))
-    balance = get_ether_balance(user['ethereum'].address)
-    echo('Test Ether balance: {} ETH'.format(balance))
-    check_ether()
+    secho(user['rsa'].publickey().exportKey().decode(), fg='green')
+    echo('Bigchain public key: {}'.format(user['bigchain'].public_key))
+    # Check registered on BigChainDB
+    
+    echo("[+] Loading all records of user on the blockchain")
+    publicpatient = Patient(public_key=user['bigchain'].public_key)
+    if publicpatient.registered:
+        secho('Registered on the Blockchain!', fg='green')
+    else:
+        secho("Not yet registered on the Blockchain. Register with the 'register' command",fg='yellow')
+    echo("MedBlocks: {}".format(len(publicpatient.medblocks)))
+
+@main.command()
+
+@click.option('--to','-t', help="Mobile number of the patient")
+@click.option('--emergency','-e', help="IS EMERGENCY DATA", is_flag=True)
+@click.argument('file',type=click.Path(exists=True))
+def add(file, to, emergency):
+    echo("[+] Fetching user data from the blockchain")
+    patient = Patient(mobile=to)
+    public_rsa = patient.bio['rsa']
+    echo("[+] Got Public RSA key")
+    secho(public_rsa, fg='green')
+    public_rsa = RSA.importKey(public_rsa)
+    echo("[+] Generating random AES key")
+    key = generate_random_aes_key()
+    echo("[+] Encrypting data...")
+    enc = encrypt_file(file, key)
+    secho("[+] Writing encrypted data to IPFS", fg='yellow')
+    api = ipfs_connect()
+    ipfs_hash = api.add_bytes(enc)
+    secho("[+] Data written to IPFS: {}".format(ipfs_hash), fg='green')
+    echo("[+] Encrypting AES key with RSA public key")
+    encrypted_key = encrypt_rsa(key, public_rsa)
+    # Create MedBlock
+    click.echo([ipfs_hash, encrypted_key, patient.bio['bigchain'], 'schema'])
 
 if __name__ == "__main__":
     main()
