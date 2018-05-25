ab9f1b355237809fbd26a87242093c9ca48a0544
Sidharth Ramesh <tornadoalert@gmail.com>
2018-05-25 21:51:37 +0530

    added get, permit

--- medblocks.py
+++ medblocks.py
@@ -6,6 +6,7 @@ from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair, CryptoKeypair
 import requests
 import ipfsapi
+import time
 from encryption import *
 bdb = BigchainDB(
     'https://test.bigchaindb.com',
@@ -13,6 +14,7 @@ bdb = BigchainDB(
              'app_key': '6f108c571fcb25c4d34056bede9d246f'})
 credentials_path = os.path.expanduser('~/.medblocks')
 version = '0.01'
+emergency_address = '3gk3RzXuchFZrHuPjQQb6Aeyevs16t6e2GnSh7qkTUJ1'
 
 @click.group()
 @click.version_option(version, message="MedBlocks v0.01")
@@ -111,6 +113,7 @@ class Patient(object):
         if public_key is not None:
             self.bio = self.get_bio_from_public_key(public_key)
         self.medblocks = []
+        self.non_keyed = []
         self.get_medblocks()
         
     def get_bio_from_mobile(self, mobile):
@@ -147,29 +150,98 @@ class Patient(object):
         return
 
     def get_medblocks(self):
+        """Gets all medblocks where 'patient' address is current patient's"""
         if self.records is None:
             self.records = bdb.assets.get(search=self.bio['bigchain'])
     
         for record in self.records:
-            if record.get('data').get('schema') == 'medblocks.medblock':
+            if record.get('data').get('schema') == 'medblocks.medblock' and record.get('data')['patient'] == self.bio['bigchain']:
                 self.medblocks.append(record)
 
         self.medblocks = version_filter(self.medblocks)
+        keyed_blocks = []
+        non_keyed = []
+        for medblock in self.medblocks:
+            asset_id = medblock['id']
+            keys = self.get_keys(asset_id)
+            medblock['owned'] = self.owner_is_patient(asset_id)
+            if len(keys) > 0:
+                medblock['keys']=keys
+                keyed_blocks.append(medblock)
+            else:
+                non_keyed.append(medblock)
+        
+        self.medblocks = keyed_blocks
+        self.non_keyed = non_keyed
+        
+    def has_permission(self, medblock):
+        for k in medblock['keys']:
+            if k == self.bio['bigchain']:
+                return medblock['keys'][k]
+        return False
     
-    def write_medblock(self, ipfs_hash, encrypted_key, type=None):
+    def get_owner(self, asset_id):
+        txns = bdb.transactions.get(asset_id=asset_id)
+        return txns[-1]['outputs'][0]['condition']['details']['public_key']
+    
+    def owner_is_patient(self, asset_id):
+        owner = self.get_owner(asset_id)
+        if owner == self.bio['bigchain']:
+            return True
+        else:
+            return False
+    
+    def get_keys(self, asset_id):
+        keys = {}
+        txns = bdb.transactions.get(asset_id=asset_id)
+        for tx in txns:
+            meta = tx.get('metadata')
+            if meta is not None:
+                for k in meta['keys']:
+                    keys[k] = meta['keys'][k]
+        return keys
+    
+    def display_medblocks(self):
+        for i, medblock in enumerate(self.medblocks):
+            echo("------------------------------------Medblock {}------------------------------------".format(i+1))
+            secho("ID: {}".format(medblock['id']), fg='yellow')
+            echo("IPFS hash: {}".format(medblock['data']['ipfs_hash']))
+            echo("File Format: {}".format(medblock['data']['type']))
+            keys = self.get_keys(medblock['id'])
+            if emergency_address in keys.keys():
+                secho("**EMERGENCY BLOCK**", fg='red')
+            echo("Permitted addresses: {}".format(len(keys)))
+            if self.has_permission(medblock):
+                secho("Current user can decrypt", fg='green')
+            else:
+                secho("Current user cannot decrypt")
+            echo("----------------------------------------------------------------------------------")
+
+    def write_medblock(self, ipfs_hash, encrypted_key, emergency_key=None, format=None):
         data = {
         'schema':'medblocks.medblock',
         'version':'v0.01',
-        'type': type,
+        'type': format,
         'ipfs_hash': ipfs_hash,
         'patient': self.bio['bigchain']
         }
+        metadata = {
+            'schema':'medblocks.aes', 
+            'keys': {
+                self.bio['bigchain']: encrypted_key
+                }
+        }
+
+        if emergency_key is not None:
+            metadata['keys'][emergency_address] = emergency_key
+        
         echo("[+] Preparing CREATE transaction with {}...".format(str(data)[:10]))
         tx = bdb.transactions.prepare(
         operation='CREATE',
         signers=current_user['bigchain'].public_key,
-        asset={'data': data})
-        
+        asset={'data': data},
+        metadata=metadata
+        )
         echo("[+] Signing with private key")
         signed_tx = bdb.transactions.fulfill(
         tx,
@@ -177,15 +249,11 @@ class Patient(object):
         )
         echo("[+] Creating MedBlock on the Blockchain")
         sent = bdb.transactions.send(signed_tx)
-        echo("[+] Created! {}".format(sent))
-        self.transfer_medblock(sent, encrypted_key)
+        echo("[+] Created: {}".format(sent['id']))
+        self.transfer_medblock(sent)
     
-    def transfer_medblock(self, tx, encrypted_key):
-        
+    def transfer_medblock(self, tx, metadata=None):
         echo("[+] Transfering MedBlock to patient: {}".format(self.bio['bigchain']))
-        metadata = {
-            self.bio['bigchain']: encrypted_key
-        }
         output = tx['outputs'][0]
         transfer_input = {
             'fulfillment': output['condition']['details'],
@@ -193,26 +261,45 @@ class Patient(object):
                 'output_index': 0,
                 'transaction_id': tx['id'],},
             'owners_before': output['public_keys'],}
+        if tx['operation'] == 'TRANSFER':
+            asset_id = tx['asset']['id']
+        else:
+            asset_id = tx['id']
+
         transfer_asset = {
-            'id':tx['id'],
+            'id':asset_id,
         }
         echo("[+] Preparing TRANSFER transaction with encrypted key")
-        prepared_tx = bdb.transaction.prepare(
-            operation='TRANSFER',
-            asset=transfer_asset,
-            inputs=transfer_input,
-            recipients=self.bio['bigchain'],
-            metadata=metadata
-        )
+        if metadata is None:
+            prepared_tx = bdb.transactions.prepare(
+                operation='TRANSFER',
+                asset=transfer_asset,
+                inputs=transfer_input,
+                recipients=self.bio['bigchain'],
+            )
+        else:
+            prepared_tx = bdb.transactions.prepare(
+                operation='TRANSFER',
+                asset=transfer_asset,
+                inputs=transfer_input,
+                recipients=self.bio['bigchain'],
+                metadata=metadata
+            )
         echo("[+] Signing transaction")
-        signed = bdb.transaction.fulfill(
+        signed = bdb.transactions.fulfill(
             prepared_tx,
             private_keys=current_user['bigchain'].private_key
         )
+        for attempt in range(10):
+            block = bdb.blocks.get(txid=tx['id'])
+            if block is not None:
+                echo("[+] Asset block confirmed on the blockchain: {}".format(block))
+                break
+            echo("[o] Waiting for the asset block to confirm on blockchain. Sleeping for 0.5...")
+            time.sleep(0.5)
         echo("[+] Sending on the blockchain")
         tx = bdb.transactions.send(signed)
-        echo(tx)
-        secho("[+] Transaction sent!", fg='green')
+        secho("[+] Transaction sent: {}".format(tx['id'], fg='green'))
 
 
 def ipfs_connect():
@@ -220,8 +307,8 @@ def ipfs_connect():
         api = ipfsapi.connect('127.0.0.1', 5001)
         secho("[+] Connected to local IPFS node", fg='green')
     except ipfsapi.exceptions.ConnectionError:
-        secho("[o] Local IPFS daemon not running. Connecting to ipfs.infura.io node...", fg='yellow')
-        api = ipfsapi.connect('https://ipfs.infura.io', 5001)
+        secho("[-] Local IPFS daemon not running. Exiting...", fg='red')
+        exit(1)
     return api
 
 def give_permission(address, medblock_index):
@@ -290,14 +377,33 @@ def info():
         secho("Not yet registered on the Blockchain. Register with the 'register' command",fg='yellow')
     echo("MedBlocks: {}".format(len(publicpatient.medblocks)))
 
-@main.command()
+def get_patient(address, phone):
+    if address is None and phone is None:
+        echo("[o] No address or phone number provided. Loading current logged in user")
+        user = load()
+        publicpatient = Patient(public_key=user['bigchain'].public_key)
+    if address is not None:
+        publicpatient = Patient(public_key=address)
+    if phone is not None:
+        publicpatient = Patient(mobile=phone)
+    return publicpatient
 
-@click.option('--to','-t', help="Mobile number of the patient")
+@main.command()
+@click.option('--address', '-a', help="The bigchain address of the patient")
+@click.option('--phone','-p',help="The phone numer of the patient")
+def list(address, phone):
+    """List all medblocks of current user"""
+    publicpatient = get_patient(address, phone)
+    publicpatient.display_medblocks()
+
+@main.command()
+@click.option('--phone','-t', help="Mobile number of the patient")
+@click.option('--address','-a', help="Public key of the patient")
 @click.option('--emergency','-e', help="IS EMERGENCY DATA", is_flag=True)
 @click.argument('file',type=click.Path(exists=True))
-def add(file, to, emergency):
+def add(file, phone, address, emergency):
     echo("[+] Fetching user data from the blockchain")
-    patient = Patient(mobile=to)
+    patient = get_patient(address, phone)
     public_rsa = patient.bio['rsa']
     echo("[+] Got Public RSA key")
     secho(public_rsa, fg='green')
@@ -312,9 +418,93 @@ def add(file, to, emergency):
     secho("[+] Data written to IPFS: {}".format(ipfs_hash), fg='green')
     echo("[+] Encrypting AES key with RSA public key")
     encrypted_key = encrypt_rsa(key, public_rsa)
-    # Create MedBlock
-    click.echo([ipfs_hash, encrypted_key, patient.bio['bigchain'], 'schema'])
+    encrypted_key = encrypted_key.decode()
 
+    # Create MedBlock
+    if len(file.split('.'))> 0:
+        format = file.split('.')[-1]
+    
+    if emergency:
+        emergency_account = Patient(public_key=emergency_address)
+        emergency_rsa = RSA.importKey(emergency_account.bio['rsa'])
+        emergency_key = encrypt_rsa(key, emergency_rsa)
+        emergency_key = emergency_key.decode()
+        patient.write_medblock(ipfs_hash, encrypted_key, emergency_key, format=format)
+    else:
+        patient.write_medblock(ipfs_hash, encrypted_key, format=format)
+
+@main.command()
+@click.option('--address', '-a', help="The bigchain address of the patient")
+@click.option('--phone','-p',help="The phone numer of the patient")
+@click.option('--all', is_flag=True)
+@click.argument('asset')
+def permit(asset, address, phone, all):
+        # Check if medblock belongs to user
+        doctor = get_patient(address, phone)
+        recorded_keys = doctor.get_keys(asset)
+        if doctor.bio['bigchain'] in recorded_keys:
+            echo("[+] Permission already granted")
+            exit()
+        
+        user_patient = Patient(public_key=current_user['bigchain'].public_key)
+        if user_patient.owner_is_patient(asset):
+            tx = bdb.transactions.get(asset_id=asset)[-1]
+            public_rsa = doctor.bio['rsa']
+            echo("[+] Got Public RSA key")
+            secho(public_rsa, fg='green')
+            public_rsa = RSA.importKey(public_rsa)
+            
+            encrypted_key = recorded_keys[user_patient.bio['bigchain']]
+            # Decrypt key
+            echo("[+] Decrypting AES key using private key")
+            decrypted_key = decrypt_rsa(encrypted_key.encode(), current_user['rsa'])
+            echo("[+] Decrypted : {}".format(decrypted_key))
+            click.pause()
+            echo("[+] Encrypting AES key with RSA public key")
+            encrypted_key = encrypt_rsa(decrypted_key, public_rsa)
+            encrypted_key = encrypted_key.decode()
+            user_patient.transfer_medblock(tx,metadata={
+                                            'schema':'medblocks.aes', 
+                                            'keys': {
+                                                doctor.bio['bigchain']: encrypted_key
+                                                }
+                                        })
+        else:
+            echo("[-] Current user not owner of medblock. Please login with a different account")
+@main.command()
+@click.argument('asset')
+@click.option('--output','-o',type=click.Path(), help='Output path for file')
+def get(asset, output):
+    user_patient = Patient(public_key=current_user['bigchain'].public_key)
+    keys = user_patient.get_keys(asset)
+    if current_user['bigchain'].public_key in keys.keys():
+        secho("[+] Current user has permission to decrypt", fg='green')
+        data = bdb.transactions.get(asset_id=asset)[0]['asset']['data']
+        
+        
+        echo("[+] Decrypting AES key")
+        encrypted_key = keys[current_user['bigchain'].public_key]
+        encrypted_key = encrypted_key.encode()
+        key = decrypt_rsa(encrypted_key, current_user['rsa'])
+        echo("[+] Retrtriving file from IPFS")
+        api = ipfs_connect()
+        encrypted = api.cat(data['ipfs_hash'])
+        echo("[+] Decrypting file")
+        decrypted = decrypt(encrypted, key)
+        secho("[+] File decrypted", fg='green')
+        echo("Length: {}".format(len(decrypted)))
+        echo("...Head display...", nl=True)
+        echo(decrypted[:600], nl=True)
+        echo("",nl=True)
+        if output is None:
+            output = '.'.join([asset, data['type']])
+        echo("[+] Writing to {}".format(output))
+        with open(output, 'wb') as f:
+            f.write(decrypted)
+        secho("[+] Done!", fg='green')
+    else:
+        secho("[+] Current user does has permission to decrypt", fg='red') 
+        exit(1)
 if __name__ == "__main__":
     main()
 
