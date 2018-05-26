ab64f1aeceae628b07bbd129fade95ccc96e3648
Sidharth Ramesh <tornadoalert@gmail.com>
2018-05-26 10:25:27 +0530

    basic version changes

---
+++ README.md
@@ -0,0 +1,1 @@
+# MedBlocks
--- medblocks.py
+++ medblocks.py
@@ -17,7 +17,7 @@ version = '0.01'
 emergency_address = '3gk3RzXuchFZrHuPjQQb6Aeyevs16t6e2GnSh7qkTUJ1'
 
 @click.group()
-@click.version_option(version, message="MedBlocks v0.01")
+@click.version_option(version, message=version)
 def main():
     """
     \b
